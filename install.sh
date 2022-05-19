78e6af2fe97cef3011055592a8dc50ca30388ae1
Doge815 <42283470+Doge815@users.noreply.github.com>
2022-05-19 16:31:06 +0200

    run script as user

--- install.sh
+++ install.sh
@@ -2,8 +2,4 @@
 path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$path"
 /bin/cp -rf home/. ~
-if ! [ $(id -u) = 0 ]; then
-	echo "This script must be run as root to install all config files" 
-else
-	/bin/cp -rf root/. /
-fi
+doas /bin/cp -rf root/. /
